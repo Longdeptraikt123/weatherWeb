33d4ea5c9cff19d00154e71a4144af91dc136aaa
Long <laptoptandt@Longs-Macbookpro.local>
2023-08-27 20:28:35 +0700

    update react-query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header'
 import Content from './components/content'
-import { createContext, useState } from 'react';
-// import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { createContext, useLayoutEffect, useState } from 'react';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
+import axios from 'axios'
 export const WeatherContext = createContext(null)
 function App() {
-    // const queryClient = useQueryClient()
+    const queryClient = useQueryClient();
+
     const dateBuilder = (d) => {
         let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
         let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
@@ -18,37 +20,54 @@ function App() {
     }
 
     const [country, setCountry] = useState('')
-    const [weatherData, setWeatherData] = useState({})
-
 
     const fetchData = async (city) => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=6573ed0b8c8d4a642450eba6bcf8009a`
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=6573ed0b8c8d4a642450eba6bcf8009a`;
 
         try {
             const response = await fetch(url);
             const data = await response.json();
-            setWeatherData(data);
-            return data
-        }
-        catch (error) {
+            console.log(data);
+            return data;
+        } catch (error) {
             console.log(error);
         }
     };
 
+    const { isLoading, data: weatherData } = useQuery({
+        queryFn: () => fetchData(),
+        queryKey: ["weatherData"],
+        staleTime: Infinity,
+        cacheTime: 0
+    })
 
-    // const { isLoading, data: weatherData } = useQuery({
-    //     queryFn: () => fetchData(),
-    //     queryKey: ["weatherData"],
-    //     staleTime: Infinity,
-    //     cacheTime: 0
-    // })
+
+    const updateWeatherData = async (newCity) => {
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=imperial&APPID=6573ed0b8c8d4a642450eba6bcf8009a`);
+            return response.data;
+        } catch (error) {
+            throw new Error(error.response.data.message || 'An error occurred');
+        }
+    };
+
+    // Sử dụng useMutation
+
+    const mutation = useMutation(updateWeatherData, {
+        onSuccess: (data) => {
+            queryClient.setQueryData(["weatherData"], data); // Cập nhật dữ liệu trong cache
+        },
+    });
 
     const search = (e) => {
         if (e.key === 'Enter') {
-            fetchData(country);
-            setCountry('')
+            // Gọi mutation để thay đổi dữ liệu
+            mutation.mutate(country);
+            setCountry('');
         }
     };
+
+    console.log(weatherData);
 
     const value = {
         dateBuilder,
@@ -57,6 +76,12 @@ function App() {
         setCountry,
         weatherData
     }
+
+    if (isLoading) {
+        return <span>Loading...</span>
+
+    }
+
 
     return (
         <WeatherContext.Provider value={value}>
